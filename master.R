a999985a490bee12b46cc72e29003e9a3497dc6f
Jayden Jung <83850482+jj-andj@users.noreply.github.com>
2023-02-16 02:15:36 -0500

    Adding code for packages

--- master.R
+++ master.R
@@ -1,3 +1,20 @@
+### Setup ###
+install.packages("readstata13") 
+install.packages("MASS")
+install.packages("sandwich")
+install.packages("lmtest")
+install.packages("stargazer")
+install.packages("list")
+install.packages("pBrackets")
+
+library(readstata13)
+library(MASS)
+library(sandwich)
+library(lmtest)
+library(stargazer)
+library(list)
+library(pBrackets)
+
 # Main Analysis in the paper
 source("ContextAnalysis_Main.R")
 source("SurveyAnalysis_Main.R")
